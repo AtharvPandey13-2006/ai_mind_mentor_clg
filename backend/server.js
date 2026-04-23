a6e18677017a53b58547bdcd3998d8295db0aa6e
Atharv Pandey <atharvpandey065@gmail.com>
2026-04-23 10:52:53 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,7 @@ const app = express();
 const PORT = 5000;
 
 // Middleware
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
